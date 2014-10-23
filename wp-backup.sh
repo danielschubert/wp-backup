f8560aa0ea6c3b00ff64c3b3a3d3d8cdbc1f3998
Daniel Schubert <mail@schubertdaniel.de>
2014-10-23 21:14:07 +0200

    initial commit

---
+++ wp-backup.sh
@@ -0,0 +1,36 @@
+# Creates Backup from a Wordpress Site and Database
+# Author: Daniel Schubert <mail@schubertdaniel.de>
+# last change : 29. Sept. 2014
+
+function db_backup {
+	
+	# Vars
+	host=<host>
+	user=<username>
+	pass=<password>
+	db=<databas>
+
+	echo 'DB Export ------> '
+	mysqldump --opt --add-drop-table -h$host -u$user -p$pass $db | gzip > wp-db.sql.gz
+}
+
+function file_backup {
+	backupfile="wordpress-backup.tar"
+
+	echo 'File Backup --- building tar File...'
+	tar -cf $backupfile ../wp*
+	echo '+DONE'
+}
+
+
+echo '-------------------------------------------'
+echo 'Awesome Wordpress Backup Script'
+echo '-------------------------------------------'
+
+backupdir=backup
+
+mkdir $backupdir
+cd $backupdir
+
+db_backup
+file_backup
